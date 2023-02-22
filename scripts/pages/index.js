e3e3001ad5cdb1d6fbf3af0dd099ef23c88b88c3
Mehdi Raheemun <mraheemu@student.42.fr>
2023-02-22 18:46:47 +0100

    Step 2

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,13 @@
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        //Récupération des données depuis le json et affichage dans la console.
+        return fetch('data/photographers.json')
+            .then(response => response.json())
+            .then(data => {
+                const photographers = data.photographers;
+                console.log(`Photograhers: ${JSON.stringify(photographers)}`)
+                return photographers
+            })
+            .catch(error => console.log(error));
     }
 
     async function displayData(photographers) {
@@ -38,7 +22,7 @@
 
     async function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
+        const photographers = await getPhotographers();
         displayData(photographers);
     };
     
