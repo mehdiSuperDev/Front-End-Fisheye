236a4c38cd4910a28f8f6be18c7050ee4da5a0cd
Mehdi Raheemun <mraheemu@student.42.fr>
2023-02-23 15:52:01 +0100

    ajout adresse, citation, tjm

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,7 +1,11 @@
 function photographerFactory(data) {
     const { 
-        name, 
-        portrait 
+        name,
+        city,
+        country, 
+        portrait,
+        tagline,
+        price
     } = data;
 
     console.log(`photographerFactory: {data}`);
@@ -10,12 +14,30 @@ function photographerFactory(data) {
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
+
+        //Créer la localisation
+        const p_localisation = document.createElement('p');
+        p_localisation.textContent = `${country}, ${city}`;
+        article.appendChild(p_localisation);
+
+        //Créer la citation (tagline)
+        const p_tagline = document.createElement('p');
+        p_tagline.textContent = tagline;
+        article.appendChild(p_tagline);
+
+        //Créer le TJM (price)
+        const p_price = document.createElement('p');
+        p_price.textContent = `${price}€/jour`;
+        article.appendChild(p_price);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
