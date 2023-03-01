940452dc4942e36ee10c7e367a0eb467c9881dcb
Mehdi Raheemun <mraheemu@student.42.fr>
2023-03-01 22:19:34 +0100

    mise en place media carte (sans image/video)

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,5 +1,14 @@
 //Mettre le code JavaScript lié à la page photographer.html
 
+// JSON
+// "id": 75902334,
+// "photographerId": 82,
+// "title": "Art Mine",
+// "image": "Art_Mine.jpg",
+// "likes": 75,
+// "date": "2019-11-25",
+// "price": 55
+
 //Comment utiliser la fonction du index.js directemment ?
 async function getPhotographers() {
     //Récupération des données depuis le json et affichage dans la console.
@@ -14,6 +23,21 @@ async function getPhotographers() {
 }
 //
 
+async function getMedias(photographerId) {
+    return fetch('data/photographers.json')
+        .then(response => response.json())
+        .then(data => {
+            const medias = data.media;
+            console.log(`medias: ${medias}`);
+
+            const p = medias.filter(element => element.photographerId == photographerId);
+            console.log(`p: ${p}`);
+
+            return medias.filter(element => element.photographerId == photographerId);
+        })
+        .catch(error => console.log(error));
+}
+
 async function fillHeader(photographer) {
     const name = document.querySelector(".photograph-header h2");
     name.textContent = photographer.name;
@@ -25,9 +49,70 @@ async function fillHeader(photographer) {
     quote.textContent = photographer.tagline;
 
     const img = document.querySelector(".photograph-header img");
+    img.setAttribute("alt", photographer.name);
+    img.setAttribute("role", "img");
+
     const picture = `assets/photographers/${photographer.portrait}`;
     img.setAttribute("src", picture);
 }
+
+function mediaFactory(data) {
+    const {
+        id,
+        photographerId,
+        title,
+        image,
+        likes,
+        date,
+        price
+    } = data;
+
+    const picture = `assets/photographers/${image}`;
+
+    console.log(`pictures: ${picture}`);
+    
+    function getMediaCardDOM() {
+        const userCardDOM = document.createElement("article");
+        userCardDOM.setAttribute("tabindex", 0);
+        userCardDOM.setAttribute("aria-label", `${title}`);
+        userCardDOM.setAttribute("title", "title");
+
+        const img = document.createElement("img");
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", "");
+        img.setAttribute("role", "img");
+
+        const sectionFooter = document.createElement("div");
+        sectionFooter.setAttribute("class", "medias_section__footer");
+
+        const p_title = document.createElement("p");
+        p_title.textContent = title;
+        
+        const p_likes = document.createElement("p");
+        p_likes.textContent = `${likes} ♥️`;
+
+        sectionFooter.appendChild(p_title);
+        sectionFooter.appendChild(p_likes);
+
+        userCardDOM.appendChild(img);
+        userCardDOM.appendChild(sectionFooter);
+
+        return userCardDOM;
+    }
+    return { getMediaCardDOM };
+}
+
+async function displayMedias(medias) {
+    const mediasSection = document.querySelector(".medias_section");
+
+    console.log(`mediaSection: ${mediasSection}`);
+    
+    medias.forEach((media) => {
+        const mediaModel = mediaFactory(media);
+        const mediaCardDom = mediaModel.getMediaCardDOM();
+        mediasSection.appendChild(mediaCardDom);
+    });
+};
 
 async function init() {
     const photographers = await getPhotographers();
@@ -46,6 +131,12 @@ async function init() {
     console.log(`name photographer: ${photographers[index].name}`);
 
     fillHeader(photographers[index]);
+
+    const medias = await getMedias(photographers[index].id);
+
+    console.log(`medias: ${medias}`);
+
+    displayMedias(medias);
 };
 
 init();
