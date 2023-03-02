768636e5a046c85064d6a107c2cb04ff8192640c
Mehdi Raheemun <mraheemu@student.42.fr>
2023-03-02 11:58:21 +0100

    Mise en place basique Modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,20 @@
+const body = document.querySelector("body");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+})
+
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
+
+    body.style.overflow = "hidden";
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
+
+    body.style.overflow = "auto";
 }
