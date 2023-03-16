6cc8f19d6eee43d194165a3d44f5fda6f8bb6e22
Mehdi Raheemun <mraheemu@student.42.fr>
2023-03-16 12:24:09 +0100

    logique lightbox dans fichier à part

---
+++ scripts/pages/lightbox.js
@@ -0,0 +1,92 @@
+var slideIndex = 0;
+
+export function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+export function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    if (n > slides.length - 1) { slideIndex = 0 };
+    if (n < 0) { slideIndex = slides.length - 1 };
+    for(i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
+
+function setCaptionText(text) {
+    const caption = document.querySelector("#caption");
+    caption.textContent = text;
+}
+
+function createCaptionContainer() {
+    const captionContainer = document.createElement("div");
+    captionContainer.classList.add("caption-container");
+    return captionContainer;
+}
+
+export function createSlides(mediaElements) {
+    for (const element of mediaElements) {
+        const div = document.createElement("div");
+        div.classList.add("slide");
+
+        div.style.width = "100%";
+        div.style.height = "100%";
+
+        //Ajouter element a div
+        element.style.objectFit = "cover";
+        element.style.width = "100%";
+        element.style.maxWidth = "1200px";
+        element.style.height = "100%";
+
+        const captionContainer = createCaptionContainer();
+        const caption = document.createElement("p");
+        caption.textContent = element.alt ?? element.title;
+        captionContainer.appendChild(caption);
+
+        div.appendChild(element);
+        div.appendChild(captionContainer);
+
+        const lightboxContent = document.querySelector(".lightbox-content");
+        lightboxContent.appendChild(div);
+    }
+}
+
+function listenLightboxEvent() {
+    document.querySelector(".close.cursor").addEventListener("click", closeLightboxModal);
+    document.querySelector(".prev").addEventListener("click", () => plusSlides(-1));
+    document.querySelector(".next").addEventListener("click", () => plusSlides(1));
+}
+
+export function openLightboxModal() {
+    const body = document.querySelector("body");
+    document.getElementById("lightbox").style.display = "flex";
+    body.style.overflow = "hidden";
+
+    document.addEventListener("keydown", listenArrowKeys);
+
+    listenLightboxEvent()
+}
+
+function closeLightboxModal() {
+    console.log("closeModal clicked");
+
+    const body = document.querySelector("body");
+    document.getElementById("lightbox").style.display = "none";
+    body.style.overflow = "auto";
+
+    document.removeEventListener("keydown", listenArrowKeys);
+}
+
+function listenArrowKeys(event) {
+    if (event.key === "ArrowLeft") {
+        plusSlides(-1);
+    } else if (event.key === "ArrowRight") {
+        plusSlides(1);
+    }
+}
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -10,106 +10,9 @@
 // "price": 55
 
 import { initContactForm } from "../utils/contactForm.js";
-
-//Foncitons liées à la lightbox
-
-var slideIndex = 0;
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slide");
-    if (n > slides.length - 1) { slideIndex = 0 };
-    if (n < 0) { slideIndex = slides.length - 1 };
-    for(i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex].style.display = "block";
-}
-
-function setCaptionText(text) {
-    const caption = document.querySelector("#caption");
-    caption.textContent = text;
-}
-
-function createCaptionContainer() {
-    const captionContainer = document.createElement("div");
-    captionContainer.classList.add("caption-container");
-    return captionContainer;
-}
+import { currentSlide, createSlides, openLightboxModal } from "./lightbox.js";
 
 let mediaElements = [];
-function createSlides() {
-    for (const element of mediaElements) {
-        const div = document.createElement("div");
-        div.classList.add("slide");
-
-        div.style.width = "100%";
-        div.style.height = "100%";
-
-        //Ajouter element a div
-        element.style.objectFit = "cover";
-        element.style.width = "100%";
-        element.style.maxWidth = "1200px";
-        element.style.height = "100%";
-
-        const captionContainer = createCaptionContainer();
-        const caption = document.createElement("p");
-        caption.textContent = element.alt ?? element.title;
-        captionContainer.appendChild(caption);
-
-        div.appendChild(element);
-        div.appendChild(captionContainer);
-
-        const lightboxContent = document.querySelector(".lightbox-content");
-        lightboxContent.appendChild(div);
-    }
-}
-
-function listenLightboxEvent() {
-    document.querySelector(".close.cursor").addEventListener("click", closeLightboxModal);
-    document.querySelector(".prev").addEventListener("click", () => plusSlides(-1));
-    document.querySelector(".next").addEventListener("click", () => plusSlides(1));
-}
-
-function openLightboxModal() {
-    const body = document.querySelector("body");
-    document.getElementById("lightbox").style.display = "flex";
-    body.style.overflow = "hidden";
-
-    document.addEventListener("keydown", listenArrowKeys);
-
-    listenLightboxEvent()
-}
-
-function closeLightboxModal() {
-    console.log("closeModal clicked");
-
-    const body = document.querySelector("body");
-    document.getElementById("lightbox").style.display = "none";
-    body.style.overflow = "auto";
-
-    document.removeEventListener("keydown", listenArrowKeys);
-}
-
-//END Foncitons liées à la lightbox
-
-//
-function listenArrowKeys(event) {
-    if (event.key === "ArrowLeft") {
-        plusSlides(-1);
-    } else if (event.key === "ArrowRight") {
-        plusSlides(1);
-    }
-}
-
-//
 
 function sortMedia(type) {
     const articles = Array.from(document.querySelectorAll('.medias_section__body article'));
@@ -360,7 +263,7 @@ async function init() {
     listenMenuEvent();
 
     //Ajout slide pour la lightbox
-    createSlides();
+    createSlides(mediaElements);
 };
 
 document.addEventListener("DOMContentLoaded", init);
