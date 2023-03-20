1a3bd4c4e8c10aa5b586a9c7785ec00396560958
Mehdi Raheemun <mraheemu@student.42.fr>
2023-03-20 12:41:05 +0100

    correction tri date

--- scripts/pages/mediaSort.js
+++ scripts/pages/mediaSort.js
@@ -16,6 +16,14 @@ export function sortMedia(type) {
         });
         break;
       case "date":
+        sortedArticles = articles.sort((a, b) => {
+          const dateA = new Date(a.getAttribute("date"));
+          const dateB = new Date(b.getAttribute("date"));
+          if (dateA < dateB) return -1;
+          if (dateA > dateB) return 1;
+          return 0;
+        });
+        break;
       default:
         sortedArticles = articles.sort((a, b) => b.getAttribute("likes") - a.getAttribute("likes"));
     }
